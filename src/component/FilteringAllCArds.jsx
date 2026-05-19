8f5d0d7e4ae5c35cded1734e771f0a0aaac20d4e
umayer10-AI <mdumayer10@gmail.com>
2026-05-19 10:19:04 +0600

    add

--- src/component/FilteringAllCArds.jsx
+++ src/component/FilteringAllCArds.jsx
@@ -3,15 +3,21 @@ import { getData } from '@/lib/data';
 import React, { useContext, useEffect, useState } from 'react';
 import AllCards from './AllCards';
 import { Data } from '@/context/Context';
+import { Spinner } from '@heroui/react';
+import { HiOutlineDocumentText } from 'react-icons/hi';
+import Link from 'next/link';
 
 const FilteringAllCArds = ({search}) => {
 
     const {data,setData,sort} = useContext(Data);
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true)
             const res = await getData(search);
             setData(res);
+            setLoading(false)
         };
 
         fetchData();
@@ -29,7 +35,41 @@ const FilteringAllCArds = ({search}) => {
         filterData = [...data]
     }
 
-    return <AllCards data={filterData}></AllCards>
+    if (loading) {
+        return (
+            <div className='flex justify-center items-center my-20'>
+                <div className="flex flex-col items-center gap-2">
+                    <Spinner size="xl" color="success"/>
+                    <span className="text-xl font-bold text-muted">Loding...</span>
+                </div>
+            </div>
+        );
+    }
+
+    // return <AllCards data={filterData}></AllCards>
+    return (
+        <div>
+            {
+                data.length===0 ? <div className="flex flex-col items-center justify-center py-20">
+                    <h2 className='text-7xl'><HiOutlineDocumentText /></h2>
+
+                    <h2 className="text-2xl font-bold text-slate-700 mt-6">
+                        No Data Found
+                    </h2>
+
+                    <p className="text-slate-500 mt-2">
+                        You haven’t data any appointment yet.
+                    </p>
+
+                    <Link href={'/all-appointment'} className="mt-6 bg-cyan-600 hover:bg-cyan-700 text-white px-6 py-3 rounded-xl font-semibold">
+                        All Appointments
+                    </Link>
+                </div>
+                : 
+                <AllCards data={filterData}></AllCards>
+            }
+        </div>
+    )
 };
 
 export default FilteringAllCArds;
