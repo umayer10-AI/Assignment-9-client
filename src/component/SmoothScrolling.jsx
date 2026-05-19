650a7bf4550bbd91a4be3ffed5e820123a9a37cb
umayer10-AI <mdumayer10@gmail.com>
2026-05-19 16:13:40 +0600

    add

---
+++ src/component/SmoothScrolling.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import Lenis from "lenis";
+import { useEffect } from "react";
+
+const SmoothScrolling = ({ children }) => {
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return children;
+};
+
+export default SmoothScrolling;
