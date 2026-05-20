46f8881a8ec1a6ad65ad4a875c073734004bdbe9
umayer10-AI <mdumayer10@gmail.com>
2026-05-20 15:34:34 +0600

    add

--- src/component/Nav2.jsx
+++ src/component/Nav2.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { authClient, useSession } from '@/lib/auth-client';
+import { authClient } from '@/lib/auth-client';
 import { Avatar } from '@heroui/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
